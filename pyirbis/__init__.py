6ce7b4e2168d4e158bcdb3ee7f190d7453363284
Alexey Mironov <amironov73@gmail.com>
2018-08-09 09:08:55 +0800

    Redesign appveyor.yml

--- pyirbis/__init__.py
+++ pyirbis/__init__.py
@@ -6,4 +6,12 @@
 
 __version__ = '0.0.4'
 __author__ = 'Alexey Mironov'
+__email__ = 'amironov73@gmail.com'
 
+__title__ = 'pyiribis'
+__summary__ = 'ManagedIrbis ported from C# to Python'
+__uri__ = 'http://arsmagna.ru'
+
+
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2018 Alexey Mironov'

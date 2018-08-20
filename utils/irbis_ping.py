c722bb240421a5a3ce2194898b42be5af5b83d40
Alexey Mironov <amironov73@gmail.com>
2018-08-20 11:34:00 +0800

    Pylint workaround

---
+++ utils/irbis_ping.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+import sys
+import os
+
+parent_dir = os.path.realpath(os.path.join(os.path.realpath(__file__), '..', '..'))
+sys.path.append(parent_dir)
+# print(sys.path)
+
+from pyirbis.core import IrbisConnection
+
+if len(sys.argv) != 2:
+    print('Usage: irbis_ping <connection-string>')
+    exit(-1)
+
+with IrbisConnection() as connection:
+    try:
+        connection.parse_connection_string(sys.argv[1])
+        connection.connect()
+        max_mfn = connection.get_max_mfn()
+        print("MAX MFN=", str(max_mfn), sep='')
+    except Exception as ex:
+        print(ex)
+        exit(-1)

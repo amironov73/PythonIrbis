339586f959ab114250cf6088c6dcc50a79434d08
Ruslan Ilyasovich Gilfanov <ri.gilfanov@yandex.ru>
2020-09-01 18:48:16 +0500

    Add method Record.get(key, default)

--- irbis/record.py
+++ irbis/record.py
@@ -421,6 +421,12 @@ class Record:
 
         return ''
 
+    def get(self, key, default=None):
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return default
+
     def __setitem__(self, key: int, value: 'RecordValue') -> None:
         """
         Присвоение поля или полей по указанной метке
